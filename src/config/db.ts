5d8969d85f333715c27033f54afe7417414012d5
Aman Chaudhary <saram.chaudharyaman2408@gmail.com>
2025-04-15 02:01:54 +0530

    Added files

--- src/config/db.ts
+++ src/config/db.ts
@@ -12,11 +12,11 @@ const pool = new Pool({
 });
 
 pool.on('connect', () => {
-  console.log('🟢 Connected to PostgreSQL database');
+  console.log('Connected to PostgreSQL database');
 });
 
 pool.on('error', (err) => {
-  console.error('🔴 Unexpected error on idle PostgreSQL client:', err);
+  console.error('Unexpected error on idle PostgreSQL client:', err);
   process.exit(-1);
 });
 
